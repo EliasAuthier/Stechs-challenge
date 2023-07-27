c0fa5a8d5bc65ac29e64d5421d14aff5925d22df
EliasAuthier <96313041+EliasAuthier@users.noreply.github.com>
2023-07-27 03:42:48 +0200

    Update models.py (missing comments)

--- stechsproject/cablemodems/models.py
+++ stechsproject/cablemodems/models.py
@@ -8,7 +8,11 @@ class Lease(models.Model):
 
     mac = models.CharField(max_length=45, primary_key=True)
     ip = models.CharField(max_length=45)
-    already_queried = models.BooleanField(default = False)
+    # A fines de saber a qué IP generar la consulta del agente SNMP, necesitamos tener vinculada la MAC address
+    # a un IP.
+    already_queried = models.BooleanField(default = False) 
+    # El campo "already_queried" nos indica si el SNMP con un dado IP ya fue consultado en busca de cablemodems.
+
 
 class CableModem(models.Model):
 
